fef6b5612f41f96cebdc48a038e64fe84ed68194
ndornseif <5382403+ndornseif@users.noreply.github.com>
2025-02-18 20:02:09 +0100

    Python script to generate quick constants.

---
+++ src/generator.py
@@ -0,0 +1,14 @@
+import random
+from typing import Union
+
+def random_constant(bits: int, count: int = 1) -> str:
+	constants = list()
+	for i in range(count):
+		constants.append(hex(random.randint(0, 1 << bits)))
+	return str(constants)
+
+print(f"8 bits: {random_constant(8, 4)}")
+print(f"16 bits: {random_constant(16, 4)}")
+print(f"32 bits: {random_constant(32, 4)}")
+print(f"64 bits: {random_constant(64, 4)}")
+print(f"128 bits: {random_constant(128, 4)}")
