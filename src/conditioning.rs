5f1c14c4fb3d56be16018cb7caa4d3ab64bd1226
ndornseif <5382403+ndornseif@users.noreply.github.com>
2025-02-19 21:56:49 +0100

    Optimized RS randint method.

--- src/conditioning.rs
+++ src/conditioning.rs
@@ -20,9 +20,10 @@ pub fn u64_to_double(int: u64) -> f64 {
 pub fn rs_random_int(test_rng: &mut impl RNG, lower: u64, upper: u64) -> u64 {
     assert!(upper > lower);
     let range: u64 = upper - lower;
+    let mask: u64 = u64::MAX >> (range - 1).leading_zeros();
     let mut rn;
     loop {
-        rn = test_rng.next() & (u64::MAX >> (range - 1).leading_zeros());
+        rn = test_rng.next() & mask;
         if rn < range {
             break;
         }
